1a78f124341c9d1e7725ce0e06adddc08c3b7a1e
roman@kislovs.ru <Rkislov2006>
2017-12-10 19:29:41 +0500

    lavary Laravel-menu enabled

--- resources/views/pink/navigation.blade.php
+++ resources/views/pink/navigation.blade.php
@@ -1,7 +1,7 @@
 @if($menu)
 <div class="menu classic">
     <ul id="nav" class="menu">
-        @include(env('THEME').'.CustomMenuItems', ['items'=>$menu->roots()])
+        @include(env('THEME').'.customMenuItems', ['items'=>$MyNav->roots()])
     </ul>
 </div>
 @endif
